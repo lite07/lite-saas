f78ce913c1dace3a944a92252fa89a8d42f47c8d
Zaky Yudha Rabbani <zaky.yudha@mekari.com>
2024-05-04 20:54:08 +0700

    [user-management-v1] Add additional attributes for user

--- src/models/user.go
+++ src/models/user.go
@@ -2,6 +2,8 @@ package models
 
 type User struct {
 	ID           uint   `json:"id" gorm:"primary_key"`
+	FirstName    string `json:"first_name" gorm:"no null; default:null"`
+	LastName     string `json:"lastname"`
 	Email        string `json:"email" gorm:"index:idx_email,unique"`
 	PasswordHash string `json:"password_hash"`
 }
--- src/requests/create_user.go
+++ src/requests/create_user.go
@@ -1,6 +1,8 @@
 package requests
 
 type CreateUserRequest struct {
-	Email    string `json:"email" binding:"required"`
-	Password string `json:"password" binding:"required"`
+	FirstName string `json:"first_name" binding:"required"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email" binding:"required,email"`
+	Password  string `json:"password" binding:"required"`
 }
