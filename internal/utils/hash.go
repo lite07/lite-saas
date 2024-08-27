a566fc2a305466325d9cd1c111758d393c8a0f57
Zaky Yudha Rabbani <zaky.yudha@mekari.com>
2024-08-27 17:26:57 +0700

    [session-management-v1] Add salt to the hash utils Add capability to load from .env

--- internal/utils/hash.go
+++ internal/utils/hash.go
@@ -3,11 +3,14 @@ package utils
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"fmt"
+	"os"
 )
 
 func HashSha256String(value string) string {
+	salt := os.Getenv("HASH_SALT")
 	h := sha256.New()
-	h.Write([]byte(value))
+	h.Write([]byte(fmt.Sprintf("%s:%s", salt, value)))
 	hashValue := h.Sum(nil)
 
 	return hex.EncodeToString(hashValue)
