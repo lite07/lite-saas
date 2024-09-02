01f31cfbfb8187b2fcebb417cbf6ea8c74a942c7
Zaky Yudha Rabbani <zaky.yudha@mekari.com>
2024-09-02 22:10:12 +0700

    [role-and-authorization-v1] Add database seed capabilities

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/lite07/lite-saas/internal/database"
 	"github.com/lite07/lite-saas/internal/routers"
@@ -10,6 +12,10 @@ func main() {
 	r := gin.Default()
 
 	database.ConnectDatabase()
+
+	if os.Getenv("SEED_DATABASE") == "true" {
+		database.SeedDatabase()
+	}
 
 	routers.RegisterUsersRoute(r)
 	routers.RegisterSessionsRoute(r)
