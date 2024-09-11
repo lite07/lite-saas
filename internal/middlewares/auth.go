c0121e53ca4888b869ff60e8c0bc837cafeb0bc8
Zaky Yudha Rabbani <zaky.yudha@mekari.com>
2024-09-11 09:26:53 +0700

    [role-and-authorization-v1] Remove user relation from session

--- internal/middlewares/auth.go
+++ internal/middlewares/auth.go
@@ -1,12 +1,14 @@
 package middlewares
 
 import (
+	"encoding/json"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/lite07/lite-saas/internal/database"
 	"github.com/lite07/lite-saas/internal/models"
+	"github.com/lite07/lite-saas/internal/utils"
 )
 
 func Authenticate(authorizedRoles []string) gin.HandlerFunc {
@@ -31,8 +33,22 @@ func Authenticate(authorizedRoles []string) gin.HandlerFunc {
 			return
 		}
 
+		var sessionToken models.SessionToken
+		if err := json.Unmarshal([]byte(utils.DecryptString(session.Token)), &sessionToken); err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"message": "Authorization token is invalid or expired"})
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		var user models.User
+		if err := database.DB.Find(&user).Where("Email = ?", sessionToken.UserEmail).Error; err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"message": "Authorization token is invalid or expired"})
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
 		var userRoles []models.UserRole
-		if err := database.DB.Joins("Role").Find(&userRoles).Where("UserID = ?", session.UserID).Error; err != nil || len(userRoles) == 0 {
+		if err := database.DB.Joins("Role").Find(&userRoles).Where("UserID = ?", user.ID).Error; err != nil || len(userRoles) == 0 {
 			c.JSON(http.StatusForbidden, gin.H{"message": "Authorization token is not authorized for this action."})
 			c.AbortWithStatus(http.StatusForbidden)
 			return
